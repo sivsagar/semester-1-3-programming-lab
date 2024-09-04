42a215ba4160b1ab60e37c11454c090afb239432
Siv Sagar <sivsagar29112006@gmail.com>
2024-09-04 09:27:27 +0000

    P2

--- Assignment 011/Program 02/main.c
+++ Assignment 011/Program 02/main.c
@@ -1,12 +1,20 @@
-#include<stdio.h>
+#include <stdio.h>
 /* Print alphabets from 'b' to 't' with a gap of 1 aplhabet
 output: b d f h j l n p r t
 */
-int main() {
-    
+int main()
+{
+    for (char ch = 'b'; ch <= 't'; ch += 2)
+    {
+
+        printf("%c ",ch);
+    }
+    printf("\n");
+
     return 0;
 }
 
 /* OUTPUT
 
+b d f h j l n p r t 
 */

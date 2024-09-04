9868faf41eadf60c875cc63e244b229814f20805
Siv Sagar <sivsagar29112006@gmail.com>
2024-09-04 09:49:23 +0000

    P!1

--- Assignment 011/Program 04/main.c
+++ Assignment 011/Program 04/main.c
@@ -8,9 +8,12 @@ d)  Print the following pattern with the
 */
 
 #include <stdio.h>
-int main() {
-    for (int i = 1; i <= 4; i++) {
-        for (int j = 1; j <= i; j++) {
+int main()
+{
+    for (int i = 1; i <= 4; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
             printf("%d", i);
         }
         printf("\n");

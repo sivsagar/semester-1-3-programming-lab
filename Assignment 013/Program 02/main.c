9cc6f4f6cf4f5e95f2ea6888bed7be10f90946e0
Siv Sagar <sivsagar29112006@gmail.com>
2024-09-11 09:14:17 +0000

    P!

---
+++ Assignment 013/Program 02/main.c
@@ -0,0 +1,36 @@
+/*
+a)  Print the given pattern.
+ex - 
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+*/
+
+#include <stdio.h>
+int main() {
+    int n, count = 1;
+
+    printf("Enter number of rows : ");
+    scanf("%d", &n);
+
+    for (int i = n; i >= 0; i--) {
+        for (int j = 1; j <= i; j++) {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+/*
+-----------Output------------
+Enter number of rows: 5
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+*/

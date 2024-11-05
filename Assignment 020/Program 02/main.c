2c2374fcda699721f0fb9c8ec80cd7968e9fdac4
Siv Sagar <sivsagar29112006@gmail.com>
2024-11-05 05:01:29 +0000

    Ariyan maqsad mtt bhul na

---
+++ Assignment 020/Program 02/main.c
@@ -0,0 +1,26 @@
+/*Write a c program to count the total number of duplicate elements in an array.*/
+#include <stdio.h>
+int countDuplicates(int arr[], int size) {
+    int count = 0;
+    for (int i = 0; i < size; i++) {
+        for (int j = i + 1; j < size; j++) {
+            if (arr[i] == arr[j]) {
+                count++;
+                break;
+            }
+        }
+    }
+    return count;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 2, 3, 4, 5, 6, 1};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int duplicates = countDuplicates(arr, size);
+    printf("Total number of duplicate elements: %d\n", duplicates);
+    return 0;
+}
+
+
+/*-------------OUTPUT------------
+Total number of duplicate elements: 3*/

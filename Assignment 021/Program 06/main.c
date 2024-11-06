805a30da2b3f278f11ae7d440b49a50afaec4df3
Siv Sagar <sivsagar29112006@gmail.com>
2024-11-06 10:38:10 +0000

    push

---
+++ Assignment 021/Program 06/main.c
@@ -0,0 +1,21 @@
+/*
+f)  Declare an array of size 10. Take elements from the keyboard. 
+    Delete the element from a position (taken from user) and print the final array.
+*/
+
+#include <stdio.h>
+int main() {
+    int sz=10;
+    int a[sz];
+    printf("Enter 10 elements...");
+    for(int i=0;i<sz;i)
+    return 0;
+}
+
+/*
+-----------Output-------------------------
+Enter 10 numbers...
+1 2 3 4 5 6 7 8 9 10
+Reversed array...
+10 9 8 7 6 5 4 3 2 1
+*/

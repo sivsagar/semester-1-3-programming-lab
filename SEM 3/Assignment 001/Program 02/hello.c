93cf09a6455da2dce403e2b2454651f41e126ad7
Siv Sagar <sivsagar29112006@gmail.com>
2025-08-05 04:39:48 +0000

    push

--- SEM 3/Assignment 001/Program 02/hello.c
+++ SEM 3/Assignment 001/Program 02/hello.c
@@ -3,14 +3,29 @@
 /* To find the roots of a quadratic equation ax^2 + bx + c = 0 */
 
 int main() {
-    double a, b, c, d;
-    printf("Enter a, b, c: ");
-    scanf("%lf%lf%lf", &a, &b, &c);
-    d = b*b - 4*a*c;
-    if (d >= 0)
-        printf("Roots: %.2lf, %.2lf\n", (-b+sqrt(d))/(2*a), (-b-sqrt(d))/(2*a));
-    else
-        printf("Roots: %.2lf+%.2lfi, %.2lf-%.2lfi\n", -b/(2*a), sqrt(-d)/(2*a), -b/(2*a), sqrt(-d)/(2*a));
+    double a, b, c, discriminant, root1, root2, realPart, imagPart;
+    printf("Enter coefficients a, b and c: ");
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    discriminant = b * b - 4 * a * c;
+
+    if (discriminant > 0) {
+        root1 = (-b + sqrt(discriminant)) / (2 * a);
+        root2 = (-b - sqrt(discriminant)) / (2 * a);
+        printf("Roots are real and different.\n");
+        printf("Root 1 = %.2lf\n", root1);
+        printf("Root 2 = %.2lf\n", root2);
+    } else if (discriminant == 0) {
+        root1 = root2 = -b / (2 * a);
+        printf("Roots are real and same.\n");
+        printf("Root 1 = Root 2 = %.2lf\n", root1);
+    } else {
+        realPart = -b / (2 * a);
+        imagPart = sqrt(-discriminant) / (2 * a);
+        printf("Roots are complex and different.\n");
+        printf("Root 1 = %.2lf + %.2lfi\n", realPart, imagPart);
+        printf("Root 2 = %.2lf - %.2lfi\n", realPart, imagPart);
+    }
     return 0;
 }
 
